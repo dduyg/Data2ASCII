ef8b19baff0d3ab3f692dcb1162d2bc3081be96b
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-09-24 16:27:21 +0200

    Update data2ascii_app.py

--- data2ascii_app.py
+++ data2ascii_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Set backend before importing pyplot
 import matplotlib.pyplot as plt
 import seaborn as sns
 from io import StringIO
